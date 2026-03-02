3d5be6e9af6fea867edca5201812bb3917bac755
ManauTunjae <manau.tunjae@hotmail.com>
2026-03-02 13:46:40 +0100

    feat: add PUT to update an artist by ID

--- routes/artistRouter.js
+++ routes/artistRouter.js
@@ -18,7 +18,7 @@ artistRouter.get('/:id', async (req, res) => {
     try {
         const artistId = req.params.id;
         if (isNaN(artistId)) {
-            return res.status(400).json({ message: "Artist ID is not a valid number."});
+            return res.status(400).json({ message: "Artist ID is not a valid number!"});
         }
         const artist = await Artist.findOne({ id: artistId });
         if (!artist) {
@@ -42,6 +42,31 @@ artistRouter.post('/', async (req, res) => {
         }
         const newArtist = await Artist.create(req.body);
         res.status(201).json(newArtist);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// Update artist by ID
+artistRouter.put('/:id', async (req, res) => {
+    try {
+        const { name } = req.body;
+        const artistId = req.params.id;
+        if (isNaN(artistId)) {
+            return res.status(400).json({ message: "Artist ID has be a valid number!"});
+        }
+        if (name && typeof name !== "string") {
+            return res.status(400).json({ message: "Update artist name has to be string!"});
+        }
+        const updateArtist = await Artist.findOneAndUpdate(
+            { id: artistId },
+            req.body,
+            { new: true, runValidators: true }
+        );
+        if (!updateArtist) {
+            return res.status(404).json({message: "Artist nor found."});
+        }
+        res.status(200).send(updateArtist);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
