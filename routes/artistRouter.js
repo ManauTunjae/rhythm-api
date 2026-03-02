6d0d72c59714624fa9e2284ad50c7e00e49f9ac7
ManauTunjae <manau.tunjae@hotmail.com>
2026-03-02 12:46:37 +0100

    feat: add GET artist by ID with validation and 404 handling

--- routes/artistRouter.js
+++ routes/artistRouter.js
@@ -13,4 +13,21 @@ artistRouter.get('/', async (req, res) => {
     }
 });
 
+// Get a artist by ID
+artistRouter.get('/:id', async (req, res) => {
+    try {
+        const artistId = req.params.id;
+        if (isNaN(artistId)) {
+            return res.status(400).json({ message: "Artist ID is not a valid number."});
+        }
+        const artist = await Artist.findOne({ id: artistId });
+        if (!artist) {
+            return res.status(404).json({ message: "Artist not found." });
+        }
+        res.status(200).json(artist);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 export default artistRouter;
