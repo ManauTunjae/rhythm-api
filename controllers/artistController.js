a5663c5c7e5a2b479c13f75a88fb75c486c615e0
ManauTunjae <manau.tunjae@hotmail.com>
2026-03-10 10:46:52 +0100

    feat: update updateArtistById in artistController.js

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -10,33 +10,63 @@ export const getAllArtists = async (req, res) => {
 };
 
 export const getArtistById = async (req, res) => {
-    try {
-        const artistId = req.params.id;
-        if (isNaN(artistId)) {
-            return res.status(400).json({ message: "Artist ID is not a valid number!"});
-        }
-        const artist = await Artist.findOne({ id: artistId });
-        if (!artist) {
-            return res.status(404).json({ message: "Artist not found." });
-        }
-        res.status(200).json(artist);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+  try {
+    const artistId = req.params.id;
+    if (isNaN(artistId)) {
+      return res
+        .status(400)
+        .json({ message: "Artist ID is not a valid number!" });
     }
+    const artist = await Artist.findOne({ id: artistId });
+    if (!artist) {
+      return res.status(404).json({ message: "Artist not found." });
+    }
+    res.status(200).json(artist);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const createArtist = async (req, res) => {
-    try {
-        const { id, name } = req.body;
-        if(typeof id !== "number") {
-            return res.status(400).json({ message: "Artist ID has to a be number!"});
-        }
-        if (typeof name !== "string") {
-            return res.status(400).json({ message: "Artist name has to be string!"});
-        }
-        const newArtist = await Artist.create(req.body);
-        res.status(201).json(newArtist);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+  try {
+    const { id, name } = req.body;
+    if (typeof id !== "number") {
+      return res.status(400).json({ message: "Artist ID has to a be number!" });
     }
+    if (typeof name !== "string") {
+      return res.status(400).json({ message: "Artist name has to be string!" });
+    }
+    const newArtist = await Artist.create(req.body);
+    res.status(201).json(newArtist);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateArtistById = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const artistId = req.params.id;
+    if (isNaN(artistId)) {
+      return res
+        .status(400)
+        .json({ message: "Artist ID has be a valid number!" });
+    }
+    if (name && typeof name !== "string") {
+      return res
+        .status(400)
+        .json({ message: "Update artist name has to be string!" });
+    }
+    const updateArtist = await Artist.findOneAndUpdate(
+      { id: artistId },
+      req.body,
+      { new: true, runValidators: true },
+    );
+    if (!updateArtist) {
+      return res.status(404).json({ message: "Artist not found." });
+    }
+    res.status(200).send(updateArtist);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
--- routes/artistRouter.js
+++ routes/artistRouter.js
@@ -1,58 +1,43 @@
 import express from "express";
 import Artist from "../models/Artist.js";
-import { createArtist, getAllArtists, getArtistById } from "../controllers/artistController.js";
+import {
+  createArtist,
+  getAllArtists,
+  getArtistById,
+  updateArtistById,
+} from "../controllers/artistController.js";
 
 const artistRouter = express.Router();
 
 // Get all artists
-artistRouter.get('/', getAllArtists);
+artistRouter.get("/", getAllArtists);
 
 // Get a artist by ID
-artistRouter.get('/:id', getArtistById);
+artistRouter.get("/:id", getArtistById);
 
 // Create an artist
-artistRouter.post('/', createArtist);
+artistRouter.post("/", createArtist);
 
 // Update artist by ID
-artistRouter.put('/:id', async (req, res) => {
-    try {
-        const { name } = req.body;
-        const artistId = req.params.id;
-        if (isNaN(artistId)) {
-            return res.status(400).json({ message: "Artist ID has be a valid number!"});
-        }
-        if (name && typeof name !== "string") {
-            return res.status(400).json({ message: "Update artist name has to be string!"});
-        }
-        const updateArtist = await Artist.findOneAndUpdate(
-            { id: artistId },
-            req.body,
-            { new: true, runValidators: true }
-        );
-        if (!updateArtist) {
-            return res.status(404).json({message: "Artist not found."});
-        }
-        res.status(200).send(updateArtist);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
+artistRouter.put("/:id", updateArtistById);
 
 // Delete artist by ID
-artistRouter.delete('/:id', async (req, res) => {
-    try {
-        const artistId = req.params.id;
-        if (isNaN(artistId)) {
-            return res.status(400).json({ message: "Artist ID has to be a valid number!"});
-        }
-        const deleteArtist = await Artist.findOneAndDelete({ id: artistId });
-        if (!deleteArtist) {
-            return res.status(404).json({ message: "Artist not found."});
-        }
-        res.status(204).send(deleteArtist);
-    } catch (error) {
-        res.status(500).json({message: error.message });
+artistRouter.delete("/:id", async (req, res) => {
+  try {
+    const artistId = req.params.id;
+    if (isNaN(artistId)) {
+      return res
+        .status(400)
+        .json({ message: "Artist ID has to be a valid number!" });
     }
-}); 
+    const deleteArtist = await Artist.findOneAndDelete({ id: artistId });
+    if (!deleteArtist) {
+      return res.status(404).json({ message: "Artist not found." });
+    }
+    res.status(204).send(deleteArtist);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 export default artistRouter;
