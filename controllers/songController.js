0e2ae165ff8bdccf28bc4512b4ee324b9d58bed7
ManauTunjae <manau.tunjae@hotmail.com>
2026-03-10 10:05:42 +0100

    feat: update updateSongById in songCOntroller.js

--- controllers/songController.js
+++ controllers/songController.js
@@ -48,3 +48,28 @@ export const createSong = async (req, res) => {
     }
 };
 
+export const updateSongById = async (req, res) => {
+    try {
+        const { title, artist } = req.body;
+        const songId = req.params.id;
+        if (isNaN(songId)) {
+            return res.status(400).json({ message: "Song ID is not a valid number!"});
+        }
+        // Ensure taht updated song are created with datatype string
+        if ((title && typeof title !== "string") || (artist && typeof artist !== "string")) {
+            return res.status(400).json({ message: "Update song and artist have to be string."});
+        }
+        // Find a match song and update data
+        const updateSong = await Song.findOneAndUpdate(
+            { id: songId }, // Find a song by id
+            req.body,
+            { new: true, runValidators: true } // Return updated song and ensure Mongoose validation is run on update
+        );
+        if (!updateSong) {
+            return res.status(404).json({ message: "Song not found." });
+        }
+        res.status(200).json(updateSong);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
--- routes/songRouter.js
+++ routes/songRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import Song from "../models/Song.js";
-import { getAllSongs, getSongById, createSong } from "../controllers/songController.js";
+import { getAllSongs, getSongById, createSong, updateSongById } from "../controllers/songController.js";
 
 const songRouter = express.Router();
 
@@ -14,31 +14,7 @@ songRouter.get('/:id', getSongById);
 songRouter.post('/', createSong);
 
 // Update a song by ID with validations
-songRouter.put('/:id', async (req, res) => {
-    try {
-        const { title, artist } = req.body;
-        const songId = req.params.id;
-        if (isNaN(songId)) {
-            return res.status(400).json({ message: "Song ID is not a valid number!"});
-        }
-        // Ensure taht updated song are created with datatype string
-        if ((title && typeof title !== "string") || (artist && typeof artist !== "string")) {
-            return res.status(400).json({ message: "Update song and artist have to be string."});
-        }
-        // Find a match song and update data
-        const updateSong = await Song.findOneAndUpdate(
-            { id: songId }, // Find a song by id
-            req.body,
-            { new: true, runValidators: true } // Return updated song and ensure Mongoose validation is run on update
-        );
-        if (!updateSong) {
-            return res.status(404).json({ message: "Song not found." });
-        }
-        res.status(200).json(updateSong);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
+songRouter.put('/:id', updateSongById);
 
 // Delete a song
 songRouter.delete('/:id', async (req, res) => {
